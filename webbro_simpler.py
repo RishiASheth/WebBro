b93a3833150ca7844d3981ae48328db4351dbc25
Rishi A Sheth <rishisheth404@gmail.com>
2023-12-04 22:20:25 +0530

    Update webbro_simpler.py
    add a warning message

--- webbro_simpler.py
+++ webbro_simpler.py
@@ -1,3 +1,15 @@
+# Dear programmer:
+# when I wrote this code,
+# only god and I knew how it worked.
+# Now, only god knows it!
+#
+# Therefore, if you are trying to optimize 
+# this routine and it fails ( most surely ),
+# please increase this counter as a
+# warning for the next person:
+#
+# total_hours_wasted_here = 53
+
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
